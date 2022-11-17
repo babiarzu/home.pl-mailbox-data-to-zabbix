5e2da0d0aa5cb660026f0b15aa04f921a106e55c
Bartosz <95976734+babiarzu@users.noreply.github.com>
2022-11-17 23:32:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from home import home_downloader
 from zabbix_sender import zabbix_sender
 
-home_downloader()
-zabbix_sender()
+if __name__ == "__main__":
+    home_downloader()
+    zabbix_sender()
